b4b6d938304675be3f7312d76c969dd19a2c116f
Jimmy Millard <millardjimmy@gmail.com>
2020-05-24 18:47:59 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,39 +1,26 @@
 class Patient
-  attr_accessor :name 
-  
+  attr_accessor :name
+
   @@all = []
-  
-  
+
   def initialize(name)
-    @name=name
+    @name = name
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
-  def new_appointment(doctor, date)
+
+  def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
+    Appointment.all.select { |appointment| appointment.patient == self }
   end
-
-              # def artists
-              #     songs.map do |song|
-              #       song.artist
-              #     end
-              #   end
 
   def doctors
-    appointments.map do |appointment|
-      appointment.doctor
-    end
+    appointments.map(&:doctor)
   end
-  
 end
-
